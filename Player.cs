4930bdbd3eccb68388801660ffb5c1faa490fd94
Alexn19 <alenoragui16@gmail.com>
2025-04-03 19:01:09 -0500

    Created Project

---
+++ Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana1_AN
+{
+    public class Player
+    {
+        public Player(string name) : base(name, 500)
+        {
+        }
+
+        public override void ShowMessage()
+        {
+            Console.WriteLine("Adventurer, you shall find the hidden treasure of the Crownguard Family or you will perish by my hand");
+
+        }
+    }
